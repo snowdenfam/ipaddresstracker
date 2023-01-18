bffb5179661466efb1dd57b251cec68b20f1a4be
snowdenfam <andre.snowden92@gmail.com>
2023-01-18 02:13:58 -0500

    I am very confused righ now ( cant get location data)

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
     document.getElementById("ip-address").innerHTML = " " + json.ip;
 }
 
-function getIP(json) {
-  document.getElementById("ip-address").innerHTML = " " + json.ip;
+function getLocation(json) {
+  document.getElementById("location").innerHTML = " " + json.location.region;
 }
 
 
@@ -14,7 +14,17 @@ script.src = "https://api.ipify.org?format=jsonp&callback=getIP";
 document.head.appendChild(script);
 
 
+var script = document.createElement("script");
+script.src = "https://api.ipify.org?format=jsonp&callback=getLocation";
+document.head.appendChild(script);
+
+
 function showIpAddress() {
   var ipAddress = document.getElementById("txtInputData").value;
   document.getElementById("ip-address").innerHTML = "" + ipAddress;
+}
+
+function showLocation() {
+  var ipAddress = document.getElementById("txtInputData").value;
+  document.getElementById("location").innerHTML = "" + location;
 }
