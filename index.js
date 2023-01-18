f8d1b51da3b98d3d4b6da3fa643f9b94e45ba463
snowdenfam <andre.snowden92@gmail.com>
2023-01-18 00:42:22 -0500

    Add function showIpAddress

--- index.js
+++ index.js
@@ -6,3 +6,9 @@
 var script = document.createElement("script");
 script.src = "https://api.ipify.org?format=jsonp&callback=getIP";
 document.head.appendChild(script);
+
+
+function showIpAddress() {
+  var originalName = document.getElementById("txtInputData").value;
+  document.getElementById("show_name").innerHTML = "Your Name is :" + originalName;
+}
