0154053b010640361829a5fc913305822dfa45d8
snowdenfam <andre.snowden92@gmail.com>
2023-01-18 21:15:19 -0500

    Add JS code comments for card constants

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const API_KEY = 'at_cu4hfDxm1O4NN25Gez1wGkXacpny2';
 const IP_API_URL = `https://geo.ipify.org/api/v1?apiKey=${API_KEY}`;
 
 
-/* define constants that match elements in the DOM, such as button, ip_address, region, timezone, and isp.*/
+/* define constants that match elements in the DOM, such as sumbitButton, ip_address, locatoin, timezone, and isp.*/
 const button = document.querySelector('.submitButtonIMG');
 const ip_address = document.querySelector('.ip-address');
 const region = document.querySelector('.location');
