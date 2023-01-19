9aec5b7fb12f5469656594ead6e6ca858dca5445
snowdenfam <andre.snowden92@gmail.com>
2023-01-18 20:44:32 -0500

    Add IPIFY contants

--- index.js
+++ index.js
@@ -29,3 +29,5 @@ function showLocation() {
   document.getElementById("location").innerHTML = "" + location;
 }
 
+const API_KEY = 'at_cu4hfDxm1O4NN25Gez1wGkXacpny2';
+const IP_API_URL = `https://geo.ipify.org/api/v1?apiKey=${API_KEY}`;
